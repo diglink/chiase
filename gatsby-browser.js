f7f5b1c92a963bac7d1b89d7b1a6f790d860e6e3
thuthuat <duc2002@gmail.com>
2019-08-13 13:15:00 +0700

    Update gatsby-browser.js

--- gatsby-browser.js
+++ gatsby-browser.js
@@ -11,3 +11,19 @@ import { ThemeProvider } from "./src/context"
 export const wrapRootElement = ({ element }) => (
     <ThemeProvider>{element}</ThemeProvider>
 )
+const perlinNoise = require('https://cdn.commento.io/js/commento.js')
+
+const insertJS = () => {
+  const addJS = (src) => {
+    const s = document.createElement(`script`)
+    s.type = `text/javascript`
+    s.src = src
+    document.getElementsByTagName(`body`)[0].appendChild(s)
+  }
+
+  addJS(perlinNoise)
+}
+
+exports.onInitialClientRender = () => {
+  insertJS()
+}
